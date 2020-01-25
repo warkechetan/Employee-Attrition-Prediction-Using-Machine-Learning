8230363a72d9c432f84562b7890c7a3c8be5effd
warkechetan <33872830+warkechetan@users.noreply.github.com>
2020-01-25 16:31:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,25 @@ def result():
     jobrole={'Sales Executive':0,'Research Scientist':1,'Laboratory Technician':2,'Manufacturing Director':3,'Healthcare 			Representative':4,'Manager':5,'Sales Representative':6,'Research Director':7,'Human Resources':8}
     educ={'Life Sciences':0,'Medical':1,'Marketing':2,'Technical Degree':3,'Human Resources':4,'Other':5}
     mar={'Single':0,'Married':1,'Divorced':2}
-    if result['distance']=="":
-	result['distance']=0
-    if result['workex']=="":
-	result['workex']=0
-    if result['income']=="":
-	result['income']=0
-    a=[[bt[result['travel']],dept[result['dept']],int(result['distance']),
-gender[result['gender']],jobrole[result['jobrole']],educ[result['educ']],int(result['workex']),
-int(result['income']),mar[result['mar']]]]
+    b=result['distance']
+    c=result['workex']
+    d=result['income']
+    
+    if b=="":
+      ib=0
+    else:
+      ib=int(b)
+    if c=="":
+      ic=0
+    else:
+      ic=int(c)
+    if d=="":
+      id1=0
+    else:
+      id1=int(d)
+
+    a=[[bt[result['travel']],dept[result['dept']],ib,
+gender[result['gender']],jobrole[result['jobrole']],educ[result['educ']],ic,id1,mar[result['mar']]]]
     model = joblib.load('model.sav')
     a=[a[0]]
     pred=model.predict(a)
